1505c5a821dc8b9cf427c7a0d44a9598e2cb0246
fcracker79 <fcracker79@gmail.com>
2017-03-26 23:12:02 +0200

    Example of using unsigned long.

---
+++ src/io/mirko/lambda/UnsignedMain.java
@@ -0,0 +1,10 @@
+package io.mirko.lambda;
+
+/**
+ * Created by mypc on 26/03/17.
+ */
+public class UnsignedMain {
+    public static void main(String ... args) throws Exception {
+        System.out.println(Integer.toUnsignedLong(-1));
+    }
+}
